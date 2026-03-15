bfab31325ca9c3975cec5c9eac6b9e301424b36b
Win1011 <1354312495@qq.com>
2026-03-15 13:48:13 +0800

    Initial Commit

---
+++ BolaBola Watch App/BolaBolaApp.swift
@@ -0,0 +1,17 @@
+//
+//  BolaBolaApp.swift
+//  BolaBola Watch App
+//
+//  Created by Nan on 3/15/26.
+//
+
+import SwiftUI
+
+@main
+struct BolaBola_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BolaBola Watch App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  BolaBola Watch App
+//
+//  Created by Nan on 3/15/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
